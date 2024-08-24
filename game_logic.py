1be9395f780fd9c6b1b0cef42b1068b075333797
ZeinTonconi <zein.tonconi@gmail.com>
2024-08-23 21:48:48 -0400

    Basic Setup

--- game_logic.py
+++ game_logic.py
@@ -29,11 +29,17 @@ def get_distance(point_a: Point2D, point_b: Point2D) -> float:
     ### ---------------------- ###
     ### SU IMPLEMENTACION AQUI ###
     ### ---------------------- ###
+
     return 0.0
 
 
 def get_impulse_vector(start_point: Point2D, end_point: Point2D) -> ImpulseVector:
-    ### ---------------------- ###
-    ### SU IMPLEMENTACION AQUI ###
-    ### ---------------------- ###
-    return ImpulseVector(0, 0)
+    
+    dx = start_point.x-end_point.x
+    dy = start_point.y-end_point.y
+
+    hipopotamo = math.sqrt(dx ** 2 + dy ** 2)
+
+    angle = math.atan2(dy, dx)
+
+    return ImpulseVector(impulse=hipopotamo, angle=angle)
